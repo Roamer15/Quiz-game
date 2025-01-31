16d43fe64d5abde01486dee0d6193681d29244c0
Roamer15 <ianseeksbeta@gmail.com>
2025-01-31 05:28:26 +0100

    All pages created

--- src/services/quizApi.js
+++ src/services/quizApi.js
@@ -1,17 +1,21 @@
-const QUIZ_URL = 'https:opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+const QUIZ_URL = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
 
 export const fetchQuestions = async () => {
     try {
-        const response = await (QUIZ_URL)
+        const response = await fetch(QUIZ_URL)
         if (!response.ok){
             throw new Error('URL not found')
         }
         const data = await response.json()
+        console.log(data)
         return data.results
+       
     }
     catch(error) {
         console.error(error)
+        // return[]
     }
+    
 }
 
 export default fetchQuestions
