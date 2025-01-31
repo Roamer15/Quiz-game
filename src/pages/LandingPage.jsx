44e28d2d55ccba54cc03a4b42cbf503fb9db64e9
Roamer15 <ianseeksbeta@gmail.com>
2025-01-31 03:44:00 +0100

    Landing Page created

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,36 @@
-
+import { useEffect,useContext,useState } from "react"
+import { QuizContext } from "../context/QuizContext"
+import { useNavigate } from "react-router"
+import { fetchQuestions } from '../services/quizApi'
+import HomeCard from '../components/HomeCard'
 
 function LandingPage() {
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleQuizPageNavigation = () => {
+        navigate('/quizpage/1')
+    }
     
+    const {setQuestions} = useContext(QuizContext)
+
+    useEffect(() => {
+      getQuestions()
+    },[])
+
+    const getQuestions = async () => {
+        const loadedQuestions = await fetchQuestions()
+        if(loadedQuestions){
+            setQuestions(loadedQuestions)
+            setLoading(true)
+        }
+       
+    }
+
+
+    return (
+        <HomeCard  isLoading={loading} handleQuizPage = {handleQuizPageNavigation}/>
+    )
 }
 
 export default LandingPage
